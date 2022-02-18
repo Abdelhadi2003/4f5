e4960d24d72309e3f9a23eb2f0af6da05790d2dd
Abdelhadi Mejdoubi <2050342@cmontmorency.qc.ca>
2022-02-17 23:51:04 -0500

    module3

--- mon_projet/echec/src/main/java/echec/frontal/FrontalEchec.java
+++ mon_projet/echec/src/main/java/echec/frontal/FrontalEchec.java
@@ -4,24 +4,20 @@ import ca.ntro.app.frontend.FrontendFx;
 import ca.ntro.app.frontend.ViewRegistrarFx;
 import ca.ntro.app.frontend.events.EventRegistrar;
 import ca.ntro.app.tasks.frontend.FrontendTasks;
+import echec.frontal.taches.initialisation;
+import echec.frontal.vues.VueFileAttente;
+import echec.frontal.vues.VueRacine;
 import ca.ntro.app.services.Window;
 import static ca.ntro.app.tasks.frontend.FrontendTasks.*;
+
+import ca.ntro.app.NtroApp;
 
 public class FrontalEchec implements FrontendFx{
 
 	@Override
 	public void createTasks(FrontendTasks tasks) {
 		// TODO Auto-generated method stub
-		tasks.task("afficherFenetre")
-		
-	    .waitsFor(window())
-	    
-	    .thenExecutes(inputs -> {
-	    	
-	    	Window window = inputs.get(window());
-	    	
-	    	  window.show();
-	    });
+		initialisation.creerTaches(tasks);
 	}
 
 	@Override
@@ -39,7 +35,12 @@ public class FrontalEchec implements FrontendFx{
 	@Override
 	public void registerViews(ViewRegistrarFx registrar) {
 		// TODO Auto-generated method stub
-		
+		registrar.registerView(VueRacine.class, "/racine.xml");
+		registrar.registerView(VueFileAttente.class, "/file_attente.xml");
+		//registrar.registerStylesheet("/dev.css");
+		registrar.registerStylesheet("/prod.css");
+		registrar.registerDefaultResources("/chaines_fr.properties");
+		registrar.registerResources(NtroApp.locale("en"),"/chaines_en.properties");
 	}
 
 }
